59dbfee50fff9661204e91c739ff65ce8c440be0
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2023-03-17 15:58:27 -0300

    Inserindo legenda

--- geragrafico.py
+++ geragrafico.py
@@ -13,9 +13,12 @@ def main():
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
 
-    #plt.plot(valores)
+    i = 1
     for serie in valores:
-       plt.plot(serie)
+        plt.plot(serie, label='Série ' + str(i))   
+        i += 1
+    plt.legend(loc='upper left')
+
 
 
     plt.show()
